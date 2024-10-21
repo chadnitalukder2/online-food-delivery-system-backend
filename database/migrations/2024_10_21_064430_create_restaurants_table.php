ba097507139331b1bdbd4061e0122640cf15ea2a
chadnitalukder2 <chadnitalukder2@gmail.com>
2024-10-21 14:30:30 +0600

    migrate database

---
+++ database/migrations/2024_10_21_064430_create_restaurants_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index();
+            $table->string('phone');
+            $table->string('email')->unique();
+            $table->string('address');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
---
+++ database/migrations/2024_10_21_071343_create_delivery_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('delivery', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->unsignedBigInteger('delivery_person_id');
+            $table->foreign('delivery_person_id')->references('id')->on('delivery_personnel')->onDelete('cascade');
+            $table->string('estimated_time')->index();
+            $table->string('status')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('delivery');
+    }
+};
